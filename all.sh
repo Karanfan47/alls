f7808765197e0d7704acee947129915bb4f3a1ae
Karanfan47 <161119235+Karanfan47@users.noreply.github.com>
2025-09-28 12:16:54 +0530

    Update all.sh

--- all.sh
+++ all.sh
@@ -20,6 +20,6 @@ if ! command -v screen &>/dev/null; then
     sudo apt update && sudo apt install -y screen
 fi
 
-screen -ls | grep gensyn | awk '{print $1}' | while read s; do screen -S "$s" -X quit; done
+screen -ls | grep gensyn | awk '{print $1}' | cut -d. -f1 | while read id; do screen -S "$id" -X quit; done
 
 screen -dmS gensyn bash -c 'bash <(curl -fsSL https://raw.githubusercontent.com/HustleAirdrops/Gensyn-Advanced-Solutions/main/s.sh)'
