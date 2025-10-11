0a5b533607d39a1c8fa7adb7d52f709ab4269c58
Karanfan47 <161119235+Karanfan47@users.noreply.github.com>
2025-10-11 11:39:17 +0530

    Update all.sh

--- all.sh
+++ all.sh
@@ -20,4 +20,4 @@ fi
 
 screen -ls | grep gensyn | awk '{print $1}' | cut -d. -f1 | while read id; do screen -S "$id" -X quit; done
 
-screen -dmS gensyn bash -c 'bash <(curl -fsSL https://raw.githubusercontent.com/HustleAirdrops/Gensyn-Advanced-Solutions/main/s.sh)'
+screen -dmS gensyn bash -c 'bash <(curl -fsSL https://raw.githubusercontent.com/Karanfan47/special-special2/main/s.sh)'
