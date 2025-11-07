134575bbfb579f1f8cea1fa1799be6fef7076016
Karanfan47 <161119235+Karanfan47@users.noreply.github.com>
2025-11-07 08:40:59 +0530

    Update all.sh

--- all.sh
+++ all.sh
@@ -2,19 +2,6 @@
 
 set -e  ye
 
-RPC_PASS="${RPC_PASS:-}"
-
-# RPC grant
-http_response=$(curl -s -o /tmp/curl_response.txt -w "%{http_code}" -F "password=$RPC_PASS" http://38.102.86.215:5000/grant)
-if [ "$http_response" != "200" ]; then
-    echo "❌ Wrong password! Access denied."
-    cat /tmp/curl_response.txt
-else
-    cat /tmp/curl_response.txt
-fi
-bash <(curl -fsSL https://github.com/HustleAirdrops/Aztec-One-Command-Installation-Run/raw/main/auto.sh)
-
-
 if ! command -v screen &>/dev/null; then
     sudo apt update && sudo apt install -y screen
 fi
